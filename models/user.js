d06bf59dd50061b5fd706e64fd6c8824d92bda07
steveny9911 <stevenywy@gmail.com>
2021-02-16 13:08:06 -0800

    remove password

--- models/user.js
+++ models/user.js
@@ -19,6 +19,7 @@ userSchema.plugin(uniqueValidator);
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
+    delete returnedObject.password;
     delete returnedObject._id;
     delete returnedObject.__v;
   },
