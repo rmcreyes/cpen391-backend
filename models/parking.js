38e04d41311a171f01e8d3c53c7dd6bc57a992a9
steveny9911 <stevenywy@gmail.com>
2021-02-24 14:46:00 -0800

    parking schema

---
+++ models/parking.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const parkingSchema = new Schema(
+  {
+    carId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Car',
+      required: true,
+      unique: true,
+    },
+    licensePlate: { type: String, required: true, unique: true },
+    meterId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Meter',
+      required: true,
+      unique: true,
+    },
+    startTime: { type: Date, required: true },
+    endTime: { type: Date },
+    isParked: { type: Boolean, required: true },
+    cost: { type: Number },
+    paid: { type: Boolean },
+  },
+  { versionKey: false }
+);
+
+parkingSchema.plugin(uniqueValidator);
+
+parkingSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Parking', parkingSchema);
